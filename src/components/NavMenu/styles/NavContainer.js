4680cdb629a510a7c89423ed5e450354914d75df
Alexis-Benamar <alexis.benamar@viacesi.fr>
2021-02-11 19:51:16 +0100

    fixed atrocious dev mishap & added a bit of styling

--- src/components/NavMenu/styles/NavContainer.js
+++ src/components/NavMenu/styles/NavContainer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const TabContainer = styled('div')`
     border-right: 1px solid black;
     height: 100vh;
-    width: 200px;
+    width: 240px;
 `
 
 export default TabContainer
--- src/components/NavMenu/styles/NavItem.js
+++ src/components/NavMenu/styles/NavItem.js
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const Tab = styled(Link)`
-  color: black;
+  color: inherit;
   display: block;
   padding: 8px 16px;
   text-decoration: none;
 
   &:hover {
-    background-color: #eee;
+    background-color: #323232;
     cursor: pointer;
   }
 `
